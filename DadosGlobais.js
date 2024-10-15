dd25ade652b912cfa65bd9fd08049cc2d8ed3e91
fcarnelose <124386850+fcarnelose@users.noreply.github.com>
2024-10-15 16:52:07 -0300

    1510

--- DadosGlobais.js
+++ DadosGlobais.js
@@ -3,9 +3,20 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 console.log(url);
 
 async function vizualizarInformacoesGlobais(){
-    const res = await fetch(url)
-    const dados = await res.json()
-    console.log(dados);
+    const res = await fetch(url);
+    const dados = await res.json();
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem 
+    ${dados.total_pessoas_mundo} de pessoas e que aproximadamente
+     ${dados.total_pessoas_conectadas} estão conectadas em
+     alguma rede social e passam em média ${dados.tempo_medio} 
+     horas conectadas`
+
+     const container = document.getElementById('graficos-container');
+     container.appendChild(paragrafo);
+
+    console.log(paragrafo);
 }
 
 vizualizarInformacoesGlobais()
